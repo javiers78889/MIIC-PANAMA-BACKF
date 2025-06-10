e5a55e29ec1c8432159caeb0ff38b0acde813f07
javiers78889 <157335485+javiers78889@users.noreply.github.com>
2025-06-10 15:38:37 -0500

    Update limiter.ts

--- src/middleware/limiter.ts
+++ src/middleware/limiter.ts
@@ -4,7 +4,7 @@ export const limiter = rateLimit({
     windowMs: 10 * 60 * 1000, // 10 minutos
     max: 5, // máximo 5 peticiones por IP
     message: {
-        error: 'Demasiadas solicitudes. Intenta nuevamente más tarde.'
+        message: 'Demasiadas solicitudes. Intenta nuevamente más tarde.'
     },
     standardHeaders: true,
     legacyHeaders: false,
@@ -15,7 +15,7 @@ export const limiterQuery = rateLimit({
     windowMs: 24 * 60 * 60 * 1000,
     max: 3, // máximo 5 peticiones por IP
     message: {
-        error: 'Ha superado su limite de tokens por día.'
+        message: 'Ha superado su limite de tokens por día.'
     },
     standardHeaders: true,
     legacyHeaders: false,
